f231819e52ce8815c99b9b7f746554d095fe8df9
Federico Menozzi <federicogmenozzi@gmail.com>
2023-11-29 08:43:46 -0500

    Add function for computing approx relative time.
    "a day ago", "three months ago", etc.

---
+++ src/formatting/time.go
@@ -0,0 +1,59 @@
+package formatting
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+type Clock interface {
+	Now() time.Time
+}
+
+type RealClock struct{}
+
+func (c *RealClock) Now() time.Time {
+	return time.Now()
+}
+
+var (
+	day   = 24 * time.Hour
+	month = 30 * day
+	year  = 12 * month
+)
+
+// Converts a timestamp from the past to a high-level human-readable time
+// string (e.g. "2 minutes ago", "5 years ago", etc).
+func GetRelativeTime(clock Clock, timestamp time.Time) string {
+	now := clock.Now()
+	if timestamp.After(now) {
+		panic(fmt.Sprintf("cannot get relative time for future time %v", timestamp))
+	}
+
+	elapsed := now.Sub(timestamp)
+
+	switch {
+	case elapsed < 30*time.Second:
+		return "a few seconds ago"
+	case elapsed < 90*time.Second:
+		return "a minute ago"
+	case elapsed < 50*time.Minute:
+		return fmt.Sprintf("%.0f minutes ago", math.Ceil(elapsed.Minutes()))
+	case elapsed < 90*time.Minute:
+		return "an hour ago"
+	case elapsed < 21*time.Hour:
+		return fmt.Sprintf("%.0f hours ago", math.Ceil(elapsed.Hours()))
+	case elapsed < 36*time.Hour:
+		return "a day ago"
+	case elapsed < 25*day:
+		return fmt.Sprintf("%.0f days ago", math.Ceil(elapsed.Hours()/24.0))
+	case elapsed < 45*day:
+		return "a month ago"
+	case elapsed < 11*month:
+		return fmt.Sprintf("%.0f months ago", math.Ceil(elapsed.Hours()/(24.0*30)))
+	case elapsed < 17*month:
+		return "a year ago"
+	default:
+		return fmt.Sprintf("%.0f years ago", math.Ceil(elapsed.Hours()/(24.0*30*12)))
+	}
+}
---
+++ src/formatting/time_test.go
@@ -0,0 +1,69 @@
+package formatting
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type FakeClock struct {
+	Current time.Time
+}
+
+func (c *FakeClock) Now() time.Time {
+	return c.Current
+}
+
+func TestRelativeTime(t *testing.T) {
+	now := time.Unix(10000000, 0)
+	clock := FakeClock{now}
+
+	fiveSecondsAgo := now.Add(-5 * time.Second)
+	fortyFiveSecondsAgo := now.Add(-45 * time.Second)
+	eightySecondsAgo := now.Add(-80 * time.Second)
+	fortyFiveMinutesAgo := now.Add(-45 * time.Minute)
+	fiftyFiveMinutesAgo := now.Add(-55 * time.Minute)
+	eightyFiveMinutesAgo := now.Add(-85 * time.Minute)
+	twoHoursAgo := now.Add(-2 * time.Hour)
+	twentyTwoHoursAgo := now.Add(-22 * time.Hour)
+	thirtyHoursAgo := now.Add(-30 * time.Hour)
+	sixDaysAgo := now.Add(-6 * day)
+	twentyOneDaysAgo := now.Add(-21 * day)
+	twentySixDaysAgo := now.Add(-26 * day)
+	fortyDaysAgo := now.Add(-40 * day)
+	tenMonthsAgo := now.Add(-10 * month)
+	twelveMonthsAgo := now.Add(-12 * month)
+	sixteenMonthsAgo := now.Add(-16 * month)
+	eighteenMonthsAgo := now.Add(-18 * month)
+	fourYearsAgo := now.Add(-4 * year)
+
+	assert.Equal(t, "a few seconds ago", GetRelativeTime(&clock, fiveSecondsAgo))
+	assert.Equal(t, "a minute ago", GetRelativeTime(&clock, fortyFiveSecondsAgo))
+	assert.Equal(t, "a minute ago", GetRelativeTime(&clock, eightySecondsAgo))
+	assert.Equal(t, "45 minutes ago", GetRelativeTime(&clock, fortyFiveMinutesAgo))
+	assert.Equal(t, "an hour ago", GetRelativeTime(&clock, fiftyFiveMinutesAgo))
+	assert.Equal(t, "an hour ago", GetRelativeTime(&clock, eightyFiveMinutesAgo))
+	assert.Equal(t, "2 hours ago", GetRelativeTime(&clock, twoHoursAgo))
+	assert.Equal(t, "a day ago", GetRelativeTime(&clock, twentyTwoHoursAgo))
+	assert.Equal(t, "a day ago", GetRelativeTime(&clock, thirtyHoursAgo))
+	assert.Equal(t, "6 days ago", GetRelativeTime(&clock, sixDaysAgo))
+	assert.Equal(t, "21 days ago", GetRelativeTime(&clock, twentyOneDaysAgo))
+	assert.Equal(t, "a month ago", GetRelativeTime(&clock, twentySixDaysAgo))
+	assert.Equal(t, "a month ago", GetRelativeTime(&clock, fortyDaysAgo))
+	assert.Equal(t, "10 months ago", GetRelativeTime(&clock, tenMonthsAgo))
+	assert.Equal(t, "a year ago", GetRelativeTime(&clock, twelveMonthsAgo))
+	assert.Equal(t, "a year ago", GetRelativeTime(&clock, sixteenMonthsAgo))
+	assert.Equal(t, "2 years ago", GetRelativeTime(&clock, eighteenMonthsAgo))
+	assert.Equal(t, "4 years ago", GetRelativeTime(&clock, fourYearsAgo))
+}
+
+func TestRelativeTimePanicsWithFutureValues(t *testing.T) {
+	now := time.Unix(10000000, 0)
+	clock := FakeClock{now}
+
+	assert.Panics(t, func() {
+		oneMonthFromNow := now.Add(month)
+		GetRelativeTime(&clock, oneMonthFromNow)
+	}, "cannot get relative time for future time")
+}
